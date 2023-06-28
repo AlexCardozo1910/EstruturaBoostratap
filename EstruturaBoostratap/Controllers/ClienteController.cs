eb1cceddbf67953373ae31a1ecd1fc5e8d22dec0
alexcardozo <alexcardozo@rocketmail.com>
2023-06-28 17:29:20 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaBoostratap/Controllers/ClienteController.cs
@@ -0,0 +1,222 @@
+﻿using EstruturaBoostratap.Data.Commun;
+using EstruturaBoostratap.Data.Models;
+using EstruturaBoostratap.ModelViews;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstruturaBoostratap.Controllers
+{
+    public class ClienteController : BasicController
+    {
+        public ActionResult Index(int page)
+        {
+            ClienteModelView dados = new ClienteModelView
+            {
+                PageAtual = page == 0 ? 1 : page
+            };
+            dados.ListaBloqueios();
+            dados.ListaClientes = dados.ListarClientes();
+
+            return View(dados);
+        }
+
+        // POST: Buscar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IFormCollection collection, int page)
+        {
+            try
+            {
+                if (collection["LimparBusca"] == "LimparFiltro")
+                {
+                    ClienteModelView reset = new ClienteModelView
+                    {
+                        PageAtual = page == 0 ? 1 : page
+                    };
+                    reset.ListaClientes = reset.ListarClientes();
+
+                    reset.ListaBloqueios();
+                    return View(reset);
+                }
+
+                ClienteModelView dados = new ClienteModelView
+                {
+                    PageAtual = page == 0 ? 1 : page,
+                    NomeClienteFiltro = collection["NomeClienteFiltro"],
+                    EmailClienteFiltro = collection["EmailClienteFiltro"],
+                    TelefoneClienteFiltro = collection["TelefoneClienteFiltro"],
+                    DataCadastroFiltro = collection["DataCadastroFiltro"],
+                    StatusFiltro = collection["StatusFiltro"]
+                };
+
+                dados.ListaClientes = dados.ListarClientes();
+
+                dados.ListaBloqueios();
+                dados.NomeClienteFiltro = collection["NomeClienteFiltro"];
+                dados.EmailClienteFiltro = collection["EmailClienteFiltro"];
+                dados.TelefoneClienteFiltro = collection["TelefoneClienteFiltro"];
+                dados.DataCadastroFiltro = collection["DataCadastroFiltro"];
+                dados.StatusFiltro = collection["StatusFiltro"];
+
+                return View(dados);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = DBModel.ViewBagMessage(ex.Message);
+                return RedirectToAction("Index", "Cliente");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            ClienteModelView dados = new ClienteModelView();
+            dados.ListaGeneros();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                ClienteModelView dados = new ClienteModelView();
+                dados.ListaGeneros();
+
+                dados.NomeCliente = collection["NomeCliente"];
+                dados.EmailCliente = collection["EmailCliente"];
+                dados.TelefoneCliente = collection["Telefone"];
+                dados.TipoPessoa = collection["TipoPessoa"];
+                dados.CpfCnpj = collection["CpfCnpj"];
+                dados.RgIe = collection["RgIe"];
+                dados.Isento = Convert.ToBoolean(collection["Isento"]);
+                dados.Genero = collection["Genero"];
+                dados.DataNascimento = collection["DataNascimento"];
+                dados.Senha = collection["Senha"];
+                dados.ConfirmarSenha = collection["ConfirmarSenha"];
+                dados.Status = Convert.ToBoolean(collection["Status"]);
+
+                if ((dados.Senha.Length < 8 || dados.Senha.Length > 15) && (dados.ConfirmarSenha.Length < 8 || dados.ConfirmarSenha.Length > 15))
+                {
+                    dados.MensagemErro = "A senha tem que conter no mínimo 8 (oito) caracteres e no máximo 15 (quinze) caracteres!";
+                    return View(dados);
+                }
+
+                if (dados.Senha != dados.ConfirmarSenha)
+                {
+                    dados.MensagemErro = "As senhas informadas não conferem!";
+                    return View(dados);
+                }
+
+                if (dados.VerificarCPF() == true)
+                {
+                    dados.MensagemErro = "Este CPF/ CNPJ já está cadastrado para outro Cliente.";
+                    return View(dados);
+                }
+
+                if (dados.VerificaEmail() == true)
+                {
+                    dados.MensagemErro = "Este e-mail já está cadastrado para outro Cliente.";
+                    return View(dados);
+                }
+
+                if (dados.TipoPessoa == "F" && dados.Isento == false)
+                {
+                    if (dados.VerificaInscricao() == true)
+                    {
+                        dados.MensagemErro = "Esta Inscrição Estadual já está cadastrada para outro Cliente.";
+                    }
+                }
+
+                dados.Senha = HashValue(collection["Senha"]);
+
+                dados.InserirCliente();
+
+                return RedirectToAction("Index", "Cliente");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = DBModel.ViewBagMessage(ex.Message);
+                return RedirectToAction("Create", "Cliente");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ClienteModelView dados = new ClienteModelView();
+            dados.CarregaInformacao(id);
+            dados.ListaGeneros();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IFormCollection collection)
+        {
+            try
+            {
+                ClienteModelView dados = new ClienteModelView();
+                dados.ListaGeneros();
+
+                dados.ClienteID = Convert.ToInt32(collection["ClienteID"]);
+                dados.NomeCliente = collection["NomeCliente"];
+                dados.EmailCliente = collection["EmailCliente"];
+                dados.TelefoneCliente = collection["Telefone"];
+                dados.TipoPessoa = collection["TipoPessoa"];
+                dados.CpfCnpj = collection["CpfCnpj"];
+                dados.RgIe = collection["RgIe"];
+                dados.Isento = Convert.ToBoolean(collection["Isento"]);
+                dados.Genero = collection["Genero"];
+                dados.DataNascimento = collection["DataNascimento"];
+                dados.Senha = collection["Senha"];
+                dados.ConfirmarSenha = collection["ConfirmarSenha"];
+                dados.Status = Convert.ToBoolean(collection["Status"]);
+
+                if (!String.IsNullOrEmpty(dados.Senha) && !String.IsNullOrEmpty(dados.ConfirmarSenha))
+                {
+                    if (dados.Senha != dados.ConfirmarSenha)
+                    {
+                        dados.MensagemErro = "As senhas informadas não conferem!";
+                        return View(dados);
+                    }
+                    dados.Senha = HashValue(collection["Senha"]);
+                }
+
+                if (dados.VerificarCPF() == true)
+                {
+                    dados.MensagemErro = "Este CPF/ CNPJ já está cadastrado para outro Cliente.";
+                    return View(dados);
+                }
+
+                if (dados.VerificaEmail() == true)
+                {
+                    dados.MensagemErro = "Este e-mail já está cadastrado para outro Cliente.";
+                    return View(dados);
+                }
+
+                if (dados.TipoPessoa == "F" && dados.Isento == false)
+                {
+                    if (dados.VerificaInscricao() == true)
+                    {
+                        dados.MensagemErro = "Esta Inscrição Estadual já está cadastrada para outro Cliente.";
+                    }
+                }
+
+                dados.AlterarCliente();
+
+                return RedirectToAction("Index", "Cliente");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = DBModel.ViewBagMessage(ex.Message);
+                return RedirectToAction("Create", "Cliente");
+            }
+        }
+    }
+}
---
+++ EstruturaBoostratap/Data/Commun/OptionServices.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstruturaBoostratap.Data.Commun
+{
+	public class OptionServices
+	{
+		public int QTDRegistros { get; set; }
+		public int PageAtual { get; set; }
+		public int RPP { get; set; } = 20;
+
+		public class NomeMeses
+		{
+			public int CodigoMeses { get; set; }
+			public string MesesNome { get; set; }
+		}
+
+		public List<NomeMeses> GetListaMeses()
+		{
+
+			var MesesReferentes = new List<NomeMeses>()
+			{
+				new NomeMeses(){ CodigoMeses = 1, MesesNome = "Janeiro" },
+				new NomeMeses(){ CodigoMeses = 2, MesesNome = "Fevereiro" },
+				new NomeMeses(){ CodigoMeses = 3, MesesNome = "Março" },
+				new NomeMeses(){ CodigoMeses = 4, MesesNome = "Abril" },
+				new NomeMeses(){ CodigoMeses = 5, MesesNome = "Maio" },
+				new NomeMeses(){ CodigoMeses = 6, MesesNome = "Junho" },
+				new NomeMeses(){ CodigoMeses = 7, MesesNome = "Julho" },
+				new NomeMeses(){ CodigoMeses = 8, MesesNome = "Agosto" },
+				new NomeMeses(){ CodigoMeses = 9, MesesNome = "Setembro" },
+				new NomeMeses(){ CodigoMeses = 10, MesesNome = "Outubro" },
+				new NomeMeses(){ CodigoMeses = 11, MesesNome = "Novembro" },
+				new NomeMeses(){ CodigoMeses = 12, MesesNome = "Dezembro" },
+			};
+
+			return MesesReferentes;
+		}
+
+		public class RegistroPagina
+		{
+			public int NumeroPagina { get; set; }
+			public string NomePagina { get; set; }
+		}
+
+		public List<RegistroPagina> GetRegistroPaginas()
+		{
+			var PaginaReferente = new List<RegistroPagina>()
+			{
+				new RegistroPagina(){ NumeroPagina = 7, NomePagina = "7 / página" },
+				new RegistroPagina(){ NumeroPagina = 14, NomePagina = "14 / página" },
+				new RegistroPagina(){ NumeroPagina = 20, NomePagina = "20 / página" },
+				new RegistroPagina(){ NumeroPagina = 30, NomePagina = "30 / página" },
+				new RegistroPagina(){ NumeroPagina = 50, NomePagina = "50 / página" },
+			};
+
+			return PaginaReferente;
+		}
+
+		public class Generos
+		{
+			public string CodigoGenero { get; set; }
+			public string NomeGenero { get; set; }
+		}
+
+		public List<Generos> GetListaGenero()
+		{
+			var dados = new List<Generos>()
+			{
+				new Generos(){ CodigoGenero = "M", NomeGenero = "Masculino" },
+				new Generos(){ CodigoGenero = "F", NomeGenero = "Feminino" },
+				new Generos(){ CodigoGenero = "O", NomeGenero = "Outros" },
+			};
+
+			return dados;
+		}
+
+		public class Bloqueio
+		{
+			public bool Valor { get; set; }
+			public string Texto { get; set; }
+		}
+
+		public List<Bloqueio> GetBloqueios()
+		{
+			var dados = new List<Bloqueio>()
+			{
+				new Bloqueio(){ Valor = false, Texto = "Sim" },
+				new Bloqueio(){ Valor = true, Texto = "Não" },
+			};
+
+			return dados;
+		}
+
+		/**
+		* Função para formatar a data de várias formas tanto para usuário quanto para o banco de dados
+		* @Author Alex Cardozo
+		* @param String tipo user['dd/mm/aaaa'], user2['dd/mm/aaaa hh:mm:ss'], bd['aaaa-mm-dd'], bd2['aaaa-mm-dd hh:mm:ss']
+		* @param data
+		* @return String
+		*/
+		public string FormatarData(string tipo, string data)
+		{
+
+			if (data == "" || data == null)
+				return data;
+
+			string data_formatada = "";
+
+			if (tipo == "user")
+			{
+				string[] data_hora = data.Split(' ');
+				string[] array_data = data_hora[0].Split('-');
+				data_formatada = array_data[2] + "/" + array_data[1] + "/" + array_data[0];
+			}
+			else if (tipo == "user2")
+			{
+				string[] data_hora = data.Split(' ');
+				string[] array_data = data_hora[0].Split('-');
+				data_formatada = array_data[2] + "/" + array_data[1] + "/" + array_data[0] + " " + data_hora[1];
+			}
+			else if (tipo == "bd")
+			{
+				string[] data_hora = data.Split(' ');
+				string[] array_data = data_hora[0].Split('/');
+				data_formatada = array_data[2] + "-" + array_data[1] + "-" + array_data[0];
+			}
+			else if (tipo == "bd2")
+			{
+				string[] data_hora = data.Split(' ');
+				string[] array_data = data_hora[0].Split('/');
+				data_formatada = array_data[2] + "-" + array_data[1] + "-" + array_data[0] + " " + data_hora[1];
+			}
+			else if (tipo == "user3")
+            {
+				string[] data_hora = data.Split(' ');
+				string[] array_data = data_hora[0].Split('/');
+				data_formatada = array_data[1] + "/" + array_data[0] + "/" + array_data[2];
+			}
+
+			return data_formatada;
+		}
+
+	}
+}
